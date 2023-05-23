e33c2aa34cee5c1b223dafa64f318aaecab14c0a
son-young-jin <sdudwls128@naver.com>
2023-05-23 12:44:34 +0900

    백준: 10814번

--- 스터디/2023 05 25/나이순 정렬.js
+++ 스터디/2023 05 25/나이순 정렬.js
@@ -1,7 +1,7 @@
-const input = ['3', '21 Junkyu', '21 Dohyun', '20 Sunyoung'];
+// const input = ['3', '21 Junkyu', '21 Dohyun', '20 Sunyoung'];
 
 const fs = require('fs');
-// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = Number(input.shift());
 const sortedMembers = input.map(el => el.split(' ')).sort((a, b) => +a[0] - +b[0]);
 const answer = sortedMembers.map(el => el.join(' ')).join('\n');
