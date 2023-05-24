812767fe3cff14f4d51fed55add7c75785145319
son-young-jin <sdudwls128@naver.com>
2023-05-24 21:32:26 +0900

    백준: 11650번

--- 스터디/2023 05 25/좌표 정렬하기.js
+++ 스터디/2023 05 25/좌표 정렬하기.js
@@ -2,11 +2,5 @@
 
 const fs = require('fs');
 const coordinates = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(el => el.split(' ').map(Number));
-
-const sortedCoordinates = coordinates.sort((a, b) => {
-  if (a[0] === b[0]) {
-    return a[1] - b[1];
-  }
-  return a[0] - b[0];
-}).map(el => el.join(' ')).join('\n');
+const sortedCoordinates = coordinates.sort((a, b) => (a[0] === b[0]) ? a[1] - b[1] : a[0] - b[0]).map(el => el.join(' ')).join('\n');
 console.log(sortedCoordinates);
