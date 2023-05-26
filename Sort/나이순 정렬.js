d3ee8328ce1962ba484214d3ce4aedf946f0af6b
son-young-jin <sdudwls128@naver.com>
2023-05-26 16:59:03 +0900

    백준: 10814번

--- Sort/나이순 정렬.js
+++ Sort/나이순 정렬.js
@@ -1,9 +1,13 @@
 // const input = ['3', '21 Junkyu', '21 Dohyun', '20 Sunyoung'];
 
+
 const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+// const input = fs.readFileSync(filePath).toString().trim().split(/\s/);
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// console.log(input);
 const n = Number(input.shift());
-const sortedMembers = input.map(el => el.split(' ')).sort((a, b) => +a[0] - +b[0]);
+const sortedMembers = input.map(el => el.split(' ')).sort((a, b) => a[0] - b[0]);
 const answer = sortedMembers.map(el => el.join(' ')).join('\n');
 
 console.log(answer);
