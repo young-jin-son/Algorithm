44e65593f69de7cd9fb6ee6ae0e65145135ce63e
son-young-jin <sdudwls128@naver.com>
2023-05-29 19:46:47 +0900

    백준 2358번

--- 스터디/2023 05 30/평행선.js
+++ 스터디/2023 05 30/평행선.js
@@ -1,4 +1,21 @@
-const input = ['4', '0 0', '10 10', '0 10', '10 0'];
-
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim();
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const n = +input[0];
+const coords = input.slice(1).map(el => el.split(' ').map(Number));
+
+const xCoordsCounter = {};
+const yCoordsCounter = {};
+let answer = 0;
+
+for (let i = 0; i < n; i++) {
+  const [x, y] = [coords[i][0], coords[i][1]];
+  (xCoordsCounter[x]) ? xCoordsCounter[x]++ : xCoordsCounter[x] = 1;
+  (yCoordsCounter[y]) ? yCoordsCounter[y]++ : yCoordsCounter[y] = 1;
+}
+
+const filteredX = Object.values(xCoordsCounter).filter(v => v > 1);
+const filteredY = Object.values(yCoordsCounter).filter(v => v > 1);
+
+answer = filteredX.length + filteredY.length;
+console.log(answer);
