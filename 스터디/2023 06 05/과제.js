8e0d0a5d6f42cfea99dbd495c74183f3ef79d5f4
son-young-jin <sdudwls128@naver.com>
2023-06-07 22:39:06 +0900

    2023 06 07

--- 스터디/2023 06 05/과제.js
+++ 스터디/2023 06 05/과제.js
@@ -1,28 +1,33 @@
 const fs = require('fs');
 const path = (process.platform === 'linux') ? '/dev/stdin' : './test.txt';
 const [n, ...input] = fs.readFileSync(path).toString().trim().split('\n');
-let sorted = input.map(el => el.split(' ').map(Number)).sort((a, b) => (a[0] === b[0]) ? b[1] - a[1] : b[0] - a[0]); // 내림차순으로 정렬
+let homeworks = input.map(el => el.split(' ').map(Number)).sort((a, b) => (a[0] === b[0]) ? b[1] - a[1] : b[0] - a[0]); // 내림차순으로 정렬
 
-console.log(sorted);
-let [day, answer] = sorted.shift();
-day--;
-let index = 1;
-// const finished = Array(Number(n)).fill(0).map((v, i) => (i) ? v : 1);
+// 첫 번째 값은 무조건 포함됨. 따라서 첫 번째 값을 day와 answer에 넣음
+let day = homeworks[0][0] - 1;
+let answer = homeworks[0][1];
+// 끝난 과제인지 아닌지를 검사하기 위한 배열, 첫 번째 과제는 이미 끝냈기 때문에 1로 표시
+const finished = Array(n).fill(0);
+finished[0] = 1;
 
-for (let i = 0; i < sorted.length; i++) {
-  if (sorted[i][0] <= day) {
-    const sliced = sorted.slice(0, i + 1);
-    const max = Math.max(sliced.map(v => v[1]));
-    const maxIndex = sorted.map(v => v[1]).indexOf(max);
-    sorted.splice(maxIndex, 1);
+for (let i = 1; i < n && day > 0; i++) {
+  if (homeworks[i][0] >= day) { // 현재 날짜보다 과제 마감일이 크거나 같을 경우에
+    let max = 0;
+    let maxIndex = 0;
+    // 끝내지 않은 과제들 중 할 수 있는 최대값과 해당 값의 위치를 찾아냄
+    for (let j = 0; j < n; j++) {
+      if (homeworks[j][0] >= day && !finished[j] && homeworks[j][1] > max) {
+        max = homeworks[j][1];
+        maxIndex = j;
+      }
+    }
+    // 최대 값을 결과에 더하고, 해당 index의 과제를 끝냈다고 표시
     answer += max;
-    console.log(answer);
-  } else {
-    sorted.splice(i, 1);
+    finished[maxIndex] = 1;
+  } else { // 현재 날짜보다 과제 마감일이 작을 경우에 i를 감소시켜서 다음 차례에도 검사할 수 있도록 함
+    i--;
   }
-  console.log(sorted);
   day--;
-  console.log('------------');
 }
 
 console.log(answer);
