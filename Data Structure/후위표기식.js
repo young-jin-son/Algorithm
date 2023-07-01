24ddfe06bc17515562c203b74b9120db511d3743
son-young-jin <sdudwls128@naver.com>
2023-07-01 09:42:26 +0900

    2023 07 01

--- Data Structure/후위표기식.js
+++ Data Structure/후위표기식.js
@@ -1,14 +1,35 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
-const medianExp = fs.readFileSync(path).toString().trim();
-const stack = [];
+const exp = fs.readFileSync(path).toString().trim();
 const answer = [];
-for (const s of medianExp) {
-  switch (s) {
-    case '(':
-      stack.push(s);
-      break;
-    case ')':
-      break;
+const stack = [];
+
+for (const v of exp) {
+  if (v === '(') {
+    stack.push(v);
+  } else if (v === ')') {
+    while (1) {
+      const popped = stack.pop();
+      if (popped === '(') break;
+      answer.push(popped);
+    }
+  } else if (/[*/]/.test(v)) {
+    if (stack.at(-1) === '*' || stack.at(-1) === '/') {
+      answer.push(stack.pop());
+    }
+    stack.push(v);
+  } else if (/[+\-]/.test(v)) {
+    while (/[+\-*/]/.test(stack.at(-1))) {
+      answer.push(stack.pop());
+    }
+    stack.push(v);
+  } else {
+    answer.push(v);
   }
 }
+
+while (stack.length) {
+  answer.push(stack.pop());
+}
+
+console.log(answer.join(''));
