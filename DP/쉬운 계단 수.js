8cb880ed0528b2c5168b50b48b1860bb11d8baa6
son-young-jin <sdudwls128@naver.com>
2023-07-03 07:37:23 +0900

    2023 07 03

--- DP/쉬운 계단 수.js
+++ DP/쉬운 계단 수.js
@@ -1,11 +1,21 @@
-const fs = require('fs');
-const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
 const n = +fs.readFileSync(path).toString().trim();
-const arr = [9];
-let tmp = 1;
+const mod = 1000000000;
+const dp = new Array(10).fill(1);
+dp[0] = 0;
+
 for (let i = 1; i < n; i++) {
-  arr.push((arr[i - 1] * 2 - tmp) % 1000000000);
-  tmp += i - 1;
-  console.log(tmp);
+  let dp_copy = [...dp];
+  for (let j = 0; j < 10; j++) {
+    if (j === 0) {
+      dp[0] = dp_copy[1] % mod;
+    } else if (j === 9) {
+      dp[9] = dp_copy[8] % mod;
+    } else {
+      dp[j] = (dp_copy[j - 1] + dp_copy[j + 1]) % mod;
+    }
+  }
 }
-console.log(arr.pop());
+
+console.log(dp.reduce((a, b) => (a + b) % mod, 0));
