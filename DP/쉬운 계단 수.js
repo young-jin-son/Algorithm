11e165ea33ba81d2100c132db822f76448c66d76
son-young-jin <sdudwls128@naver.com>
2023-07-03 14:14:09 +0900

    2023 07 03

--- DP/쉬운 계단 수.js
+++ DP/쉬운 계단 수.js
@@ -6,14 +6,14 @@ const dp = new Array(10).fill(1);
 dp[0] = 0;
 
 for (let i = 1; i < n; i++) {
-  let dp_copy = [...dp];
+  const tmp = [...dp];
   for (let j = 0; j < 10; j++) {
     if (j === 0) {
-      dp[0] = dp_copy[1] % mod;
+      dp[0] = tmp[1] % mod;
     } else if (j === 9) {
-      dp[9] = dp_copy[8] % mod;
+      dp[9] = tmp[8] % mod;
     } else {
-      dp[j] = (dp_copy[j - 1] + dp_copy[j + 1]) % mod;
+      dp[j] = (tmp[j - 1] + tmp[j + 1]) % mod;
     }
   }
 }
