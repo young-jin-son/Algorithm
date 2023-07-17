77b0851d3b21731b522f06c1c0a7bab944cb6ef1
son-young-jin <sdudwls128@naver.com>
2023-07-18 08:29:46 +0900

    2023 07 18

--- DP_/평범한 배낭.js
+++ DP_/평범한 배낭.js
@@ -6,15 +6,13 @@ const input = fs.readFileSync(path).toString().trim().split('\n');
 const sol = (input) => {
   const [n, k] = input[0].split(' ').map(Number);
   const arr = input.slice(1).map(el => el.split(' ').map(Number));
-  const dp = Array.from({ length: n + 1 }, () => Array.from({ length: k + 1 }));
+  const dp = Array.from({ length: n + 1 }, () => Array.from({ length: k + 1 }).fill(0));
 
   for (let i = 1; i <= n; i++) {
-    const [w, v] = arr[i - 1];
     for (let j = 1; j <= k; j++) {
-      // dp[i][j] = dp[i - 1][j];
-      if (j - w >= 0) {
-
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w + v]);
+      const [w, v] = arr[i - 1];
+      if (j >= w) {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
       } else {
         dp[i][j] = dp[i - 1][j];
       }
