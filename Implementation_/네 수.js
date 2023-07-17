6e3aec7df6cb68e337af7c220a8c1e9e76d9d06c
son-young-jin <sdudwls128@naver.com>
2023-07-18 08:19:12 +0900

    2023 07 18

--- Implementation_/네 수.js
+++ Implementation_/네 수.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 const [a, b, c, d] = fs.readFileSync(path).toString().trim().split(' ');
-
 console.log(Number(a + b) + Number(c + d));
