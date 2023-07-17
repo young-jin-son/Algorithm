dcd693efeed3a10a84cd89f890b917bdacb398c0
son-young-jin <sdudwls128@naver.com>
2023-07-17 22:28:19 +0900

    2023 07 17

--- DP_/평범한 배낭.js
+++ DP_/평범한 배낭.js
@@ -1,25 +1,27 @@
+// 가치 합의 최댓값 출력
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 const input = fs.readFileSync(path).toString().trim().split('\n');
 
 const sol = (input) => {
-  let [n, m] = input[0].split(' ').map(Number);
-  const arr = input.slice(1).map(el => el.split(' ').map(Number)).sort((a, b) => (a[0] === b[0]) ? b[1] - a[1] : a[0] - b[0]);
-  const dp = arr.map(v => v[1]);
-  for (let i = 1; i < n; i++) {
-    const [w, v] = arr[i];
+  const [n, k] = input[0].split(' ').map(Number);
+  const arr = input.slice(1).map(el => el.split(' ').map(Number));
+  const dp = Array.from({ length: n + 1 }, () => Array.from({ length: k + 1 }));
 
+  for (let i = 1; i <= n; i++) {
+    const [w, v] = arr[i - 1];
+    for (let j = 1; j <= k; j++) {
+      // dp[i][j] = dp[i - 1][j];
+      if (j - w >= 0) {
+
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w + v]);
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
   }
+
+  return dp[n][k];
 }
 
 console.log(sol(input));
-
-/*
-0000
-0010
-0000
-*/
-
-/*
-
-*/
---
+++ Implementation_/네 수.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const [a, b, c, d] = fs.readFileSync(path).toString().trim().split(' ');
+
+console.log(Number(a + b) + Number(c + d));
