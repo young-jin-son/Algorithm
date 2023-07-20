80e361c5f20d7a407ef52cc5c4ac9369e4ded4ad
son-young-jin <sdudwls128@naver.com>
2023-07-20 21:32:42 +0900

    2023 07 20

--- DP_/연속합.js
+++ DP_/연속합.js
@@ -3,6 +3,6 @@ const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 const [n, ...arr] = fs.readFileSync(path).toString().trim().split(/\s/g).map(Number);
 const dp = [arr[0]];
 for (let i = 1; i < n; i++) {
-  dp[i] = Math.max(arr[i], arr[i] + dp[i - 1]);
+  dp.push(Math.max(arr[i], arr[i] + dp[i - 1]));
 }
 console.log(Math.max(...dp));
