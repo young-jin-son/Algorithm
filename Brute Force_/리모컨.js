93943bc8b9c629e5f33423b2c5c795157facfca3
son-young-jin <sdudwls128@naver.com>
2023-07-27 13:28:33 +0900

    2023 07 27

--- Brute Force_/리모컨.js
+++ Brute Force_/리모컨.js
@@ -1,3 +1,24 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const [n, m, ...arr] = fs.readFileSync(path).toString().trim().split(/\s/g).map(Number);
+
+let answer = Math.abs(100 - n);
+
+for (let i = 0; i < 1_000_000; i++) {
+  let isValid = true;
+  for (const char of String(i)) {
+    if (arr.includes(+char)) {
+      isValid = false;
+      break;
+    }
+  }
+  if (isValid) {
+    answer = Math.min(answer, Math.abs(i - n) + String(i).length);
+  }
+}
+
+console.log(answer);
+
 // const fs = require('fs');
 // const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 // const [n, m, ...arr] = fs.readFileSync(path).toString().trim().split(/\s/g).map(Number);
@@ -27,24 +48,3 @@
 // }
 
 // console.log(cnt);
-
-const fs = require('fs');
-const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
-const [n, m, ...arr] = fs.readFileSync(path).toString().trim().split(/\s/g).map(Number);
-
-let answer = Math.abs(100 - n);
-
-for (let i = 0; i < 1_000_000; i++) {
-  let isValid = true;
-  for (const char of String(i)) {
-    if (arr.includes(+char)) {
-      isValid = false;
-      break;
-    }
-  }
-  if (isValid) {
-    answer = Math.min(answer, Math.abs(i - n) + String(i).length);
-  }
-}
-
-console.log(answer);
