f13cd89d4fc380576ffbf07dd1539ca370331b42
son-young-jin <sdudwls128@naver.com>
2023-07-27 20:25:16 +0900

    2023 07 27

--- DP_/가장 긴 증가하는 부분 수열 4.js
+++ DP_/가장 긴 증가하는 부분 수열 4.js
@@ -9,12 +9,14 @@ for (let i = 0; i < n; i++) {
   let max = 0;
   let maxIdx = -1;
   for (let j = 0; j < i; j++) {
+    // 증가하고, max보다 dp[j]가 크다면 
+    // => dp[j]가 현재 i번째 값이 들어갈 수 있는 부분 수열 중 최대 값이라는 뜻
     if (input[i] > input[j] && dp[j] > max) {
       max = dp[j];
       maxIdx = j;
     }
   }
-  dp[i] = max + 1;
+  dp[i] = max + 1; // max가 0 그대로여도, 들어갈 수 있는 최대값이 되었어도 + 1
   arr[i] = maxIdx !== -1 ? arr[maxIdx].concat(input[i]) : [input[i]];
 }
 
