cb421eb217491d5bba70318274177daec94083e4
son-young-jin <sdudwls128@naver.com>
2023-08-06 08:35:48 +0900

    2023 08 06

---
+++ Brute Force_/암호 만들기.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const input = fs.readFileSync(path).toString().trim().split('\n');
+
+const sol = (input) => {
+  const [l, c] = input[0].split(' ').map(Number);
+  const types = input[1].split(' ').sort();
+  const answer = [];
+  const tmp = [];
+
+  const isPassword = (pwd) => {
+    const regexConsonants = /[^aeiou]/;
+    let vowelCnt = pwd.filter(str => !regexConsonants.test(str)).length;
+    let consonantCnt = pwd.filter(str => regexConsonants.test(str)).length;
+    return (vowelCnt >= 1 && consonantCnt >= 2);
+  }
+
+  const check = (idx, cnt) => {
+    if (cnt === l) {
+      if (isPassword(tmp)) {
+        answer.push(tmp.join(''));
+      }
+      return;
+    }
+    for (let i = idx; i < c; i++) {
+      tmp.push(types[i]);
+      check(i + 1, cnt + 1);
+      tmp.pop();
+    }
+  }
+
+  check(0, 0);
+
+  return answer.join('\n');
+}
+
+console.log(sol(input));
