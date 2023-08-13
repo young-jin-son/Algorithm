0d81eef38a5933ed7ff0c79c98bd979712accd5a
son-young-jin <sdudwls128@naver.com>
2023-08-13 22:11:08 +0900

    2023 08 13

--- Greedy_/기타줄.js
+++ Greedy_/기타줄.js
@@ -1,16 +1,27 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
-const input = fs.readFileSync(path).toString().trim().split('\n');
-let [n, m] = input[0].split(' ').map(Number);
-const arr = input.slice(1).map(el => el.split(' ').map(Number));
-let answer = 0;
-const setMin = Math.min(...arr.map(el => el[0]));
-const setMinAvg = setMin / 6;
-const individualMin = Math.min(...arr.map(el => el[1]));
+const input = fs.readFileSync(path).toString().trim().split(/\s/g);
+const n = +input[0];
+const m = +input[1];
+const arr = input.slice(2).map(Number);
+const setPrice = [];
+const piecePrice = [];
 
-if (setMinAvg < individualMin) {
-  answer += Math.floor(n / 6) * setMin;
-  n -= Math.floor(n / 6) * 6;
+for (let i = 0; i < m; i++) {
+  setPrice.push(arr[2 * i]);
+  piecePrice.push(arr[2 * i + 1]);
 }
-answer += Math.min(setMin, individualMin * n);
-console.log(answer);
+
+const setMin = setPrice.reduce((acc, v) => acc < v ? acc : v, 1000);
+const pieceMin = piecePrice.reduce((acc, v) => acc < v ? acc : v, 1000);
+const setCnt = Math.floor(n / 6);
+const pieceCnt = n - 6 * setCnt;
+let cost = 0;
+
+if (setMin / 6 < pieceMin) {
+  cost = setCnt * setMin + (pieceCnt * pieceMin < setMin ? pieceCnt * pieceMin : setMin);
+} else {
+  cost = n * pieceMin;
+}
+
+console.log(cost);
