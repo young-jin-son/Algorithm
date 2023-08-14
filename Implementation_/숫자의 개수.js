06d5611656cb80831a6eb75724e6b3d642c292f6
son-young-jin <sdudwls128@naver.com>
2023-08-14 19:59:02 +0900

    2023 08 14

--- Implementation_/숫자의 개수.js
+++ Implementation_/숫자의 개수.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = process.platform === 'linux' ? 'dev/stdin' : './test.txt';
 const [a, b, c] = fs.readFileSync(path).toString().trim().split('\n').map(Number);
 const result = String(a * b * c);
-console.log(result);
 const arr = Array.from({ length: 10 }, () => 0);
 for (let i = 0; i < result.length; i++) {
   arr[+result[i]] += 1;
