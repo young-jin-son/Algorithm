7524709756005dbbba8690baa7ea8c1887a9eedb
son-young-jin <sdudwls128@naver.com>
2023-08-16 20:49:38 +0900

    2023 08 16

---
+++ Math_/다항함수의 적분.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const exp = fs.readFileSync(path).toString().trim();
+const xIdx = exp.indexOf('x');
+let answer = '';
+if (xIdx < 0) {
+  if (exp === '1') {
+    answer += 'x';
+  } else if (exp === '-1') {
+    answer += '-x';
+  } else if (exp !== '0') {
+    answer += exp + 'x';
+  }
+} else {
+  if (xIdx === 0) {
+    answer += 1 / 2;
+  } else if (exp.slice(0, xIdx) === '-') {
+    answer += -1 / 2;
+  } else {
+    answer += +(exp.slice(0, xIdx)) / 2;
+  }
+  if (answer === '1' || answer === '-1') {
+    answer = answer.slice(0, -1);
+  }
+  answer += 'xx';
+  if (exp.slice(xIdx + 1) === '+1') {
+    answer += '+x';
+  } else if (exp.slice(xIdx + 1) === '-1') {
+    answer += '-x';
+  } else if (exp.slice(xIdx + 1)) {
+    answer += exp.slice(xIdx + 1) + 'x';
+  }
+}
+
+answer += answer ? '+W' : 'W';
+
+console.log(answer);
