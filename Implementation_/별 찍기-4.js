9210d304a11e4ed05bf655260dc0a55fd7678e7c
son-young-jin <sdudwls128@naver.com>
2023-08-21 19:14:55 +0900

    2023 08 21

--- Implementation_/별 찍기-4.js
+++ Implementation_/별 찍기-4.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 const n = +fs.readFileSync(path).toString().trim();
-
-const answer = [];
 for (let i = n; i > 0; i--) {
   console.log(' '.repeat(n - i) + '*'.repeat(i));
 }
