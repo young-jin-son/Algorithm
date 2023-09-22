cd3b0a2c617ad7b7466ad799c09d82f73d083929
son-young-jin <sdudwls128@naver.com>
2023-09-23 08:45:02 +0900

    2023 09 23

--- Binary Search_/어두운 굴다리.js
+++ Binary Search_/어두운 굴다리.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 const [n, m, ...x] = fs.readFileSync(path).toString().trim().split(/\s/g).map(Number);
-
 // 첫 번째 가로등이 0까지 비출 수 있는 높이와 마지막 가로등이 n까지 비출 수 있는 높이 중 더 큰 값을 고름
 let answer = Math.max(x[0] - 0, n - x[x.length - 1]);
 for (let i = 1; i < x.length; i++) {
+  // 이전 answer 와 두 가로등 사이 거리를 2로 나눈 것 중 더 큰 값을 고름
   answer = Math.max(answer, Math.ceil((x[i] - x[i - 1]) / 2));
 }
-
 console.log(answer);
-
