cee04241a31a8d7e1f12adabc9e01868e3e9e7ea
son-young-jin <sdudwls128@naver.com>
2023-10-04 20:19:25 +0900

    2023 10 04

--- Implementation_/직사각형 네개의 합집합의 면적 구하기.js
+++ Implementation_/직사각형 네개의 합집합의 면적 구하기.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
 const input = fs.readFileSync(path).toString().trim().split('\n').map(el => el.split(' ').map(Number));
 const size = 101;
-const graph = Array.from({ length: size }, () => Array.from({ length: size }, () => 0));
+const graph = Array.from({ length: size }, () =>
+  Array.from({ length: size }, () => 0));
 // 그래프 그리기
 for (const [x1, y1, x2, y2] of input) {
   for (let i = x1; i < x2; i++) {
