ea73c153b9fdad3ed8819466b94410b90782d0a3
son-young-jin <sdudwls128@naver.com>
2023-10-23 09:40:33 +0900

    2023 10 23

---
+++ DP_/호텔.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './test.txt';
+const input = fs.readFileSync(path).toString().trim().split('\n');
+const [C, N] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map(el => el.split(' ').map(Number));
+const dp = Array.from({ length: 1001 }, () => 100);
+
+for (const [cost, customer] of arr) {
+  if (dp[customer] > cost) {
+    dp[customer] = cost;
+  }
+  for (let i = 1; i <= C; i++) {
+    dp[i] = (i < customer) ? Math.min(dp[i], cost) : Math.min(dp[i], dp[customer] + dp[i - customer]);
+  }
+}
+
+console.log(dp[C]);
