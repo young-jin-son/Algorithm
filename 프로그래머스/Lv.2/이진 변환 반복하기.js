0c8a7cdcad746923d463aa6787638c9fa7ebe782
son-young-jin <sdudwls128@naver.com>
2024-01-27 17:38:34 +0900

    2024 01 27

--- 프로그래머스/Lv.2/이진 변환 반복하기.js
+++ 프로그래머스/Lv.2/이진 변환 반복하기.js
@@ -1,14 +1,12 @@
 function solution(s) {
   let cnt = 0;
   let deletedCnt = 0;
-
   while (s !== '1') {
     cnt++;
     deletedCnt += [...s].filter(c => c === '0').length;
     s = [...s].filter(c => c !== '0').join('');
     s = s.length.toString(2);
   }
-
   return [cnt, deletedCnt];
 }
 
