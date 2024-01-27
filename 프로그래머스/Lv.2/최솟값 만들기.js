9bdcc78f37cba9ddf789d9efbd195b0c44efd69f
son-young-jin <sdudwls128@naver.com>
2024-01-27 12:43:24 +0900

    2024 01 27

---
+++ 프로그래머스/Lv.2/최솟값 만들기.js
@@ -0,0 +1,12 @@
+function solution(A, B) {
+  const sortedA = A.sort((a, b) => a - b);
+  const sortedB = B.sort((a, b) => b - a);
+  let answer = 0;
+  for (let i = 0; i < A.length; i++) {
+    answer += sortedA[i] * sortedB[i];
+  }
+  return answer;
+}
+
+console.log(solution([1, 4, 2], [5, 4, 4]));
+console.log(solution([1, 2], [3, 4]));
