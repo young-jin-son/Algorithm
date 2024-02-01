eb6305909d82dde6a0ee3a26bb5e9ad94e30b11c
son-young-jin <sdudwls128@naver.com>
2024-02-01 12:45:26 +0900

    2024 02 01

--- 프로그래머스/Lv.2/카펫.js
+++ 프로그래머스/Lv.2/카펫.js
@@ -1,9 +1,8 @@
 function solution(brown, yellow) {
   const sum = brown + yellow;
-
   let height = Math.floor(Math.sqrt(sum));
   let width = sum / height;
-  while (!Number.isInteger(width)) {
+  while (!Number.isInteger(width) || (height - 2) * (width - 2) !== yellow) {
     height -= 1;
     width = sum / height;
   }
@@ -17,4 +16,3 @@ console.log(solution(14, 4)); // [6, 3]
 console.log(solution(14, 6)); // [5, 4]
 console.log(solution(18, 6)); // [8, 3]
 console.log(solution(22, 8)); // [10, 3]
-// *3보다 크거나 같으면 가로로 된거임
