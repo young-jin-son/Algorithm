6506f98c7bb104bc1aa043f6991fd4a61e7bc6d4
son-young-jin <sdudwls128@naver.com>
2024-02-21 14:42:26 +0900

    2024 02 21

--- 프로그래머스/Lv.2/전화번호 목록.js
+++ 프로그래머스/Lv.2/전화번호 목록.js
@@ -1,15 +1,14 @@
 function solution(phone_book) {
-  phone_book.sort((a, b) => a.length - b.length);
-  for (let i = 0; i < phone_book.length; i++) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length - 1; i++) {
     const phone = phone_book[i];
-    for (let j = i + 1; j < phone_book.length; j++) {
-      const next = phone_book[j];
-      if (next.slice(0, phone.length) === phone) return false;
-    }
+    const next = phone_book[i + 1];
+    if (next.indexOf(phone) === 0) return false;
   }
   return true;
+  // return !phone_book.sort().some((value, index, self) => self[index + 1]?.indexOf(value) === 0);
 }
 
 console.log(solution(["119", "97674223", "1195524421"])); // false
 console.log(solution(["123", "456", "789"])); // true
-console.log(solution(["12", "123", "1235", "567", "88"])); // false
+console.log(solution(["12", "123", "1235", "567", "88", "1234"])); // false
