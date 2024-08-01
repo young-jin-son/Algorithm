550cb75775fa327bfedb4ff3b20af31db82b3303
son-young-jin <sdudwls128@naver.com>
2024-08-01 20:43:36 +0900

    2024 08 01

---
+++ 프로그래머스/Lv.1/바탕화면 정리.js
@@ -0,0 +1,23 @@
+function solution(wallpaper) {
+  wallpaper = wallpaper.map(el => el.split(""));
+  const rows = wallpaper.length;
+  const cols = wallpaper[0].length;
+  let [minX, minY, maxX, maxY] = [rows, cols, 0, 0];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (wallpaper[i][j] === "#") {
+        minX = Math.min(minX, i);
+        minY = Math.min(minY, j);
+        maxX = Math.max(maxX, i);
+        maxY = Math.max(maxY, j);
+      }
+    }
+  }
+  return [minX, minY, maxX + 1, maxY + 1];
+}
+
+console.log(solution([".#...", "..#..", "...#."])); // [0, 1, 3, 4]
+console.log(solution(["..........", ".....#....", "......##..", "...##.....", "....#....."])); // [1, 3, 5, 8]
+console.log(solution([".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."])); // [0, 0, 7, 9]
+console.log(solution(["..", "#."])); // [1, 0, 2, 1]
