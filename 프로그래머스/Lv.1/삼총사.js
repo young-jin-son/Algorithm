ecd24f578e26286e9f71ca63bcbc7b60907fdd00
son-young-jin <sdudwls128@naver.com>
2024-08-08 13:26:40 +0900

    2024 08 07

---
+++ 프로그래머스/Lv.1/삼총사.js
@@ -0,0 +1,17 @@
+function solution(number) {
+  let answer = 0;
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(solution([-2, 3, 0, 2, -5])); // 2
+console.log(solution([-3, -2, -1, 0, 1, 2, 3])); // 5
+console.log(solution([-1, 1, -1, 1])); // 0
