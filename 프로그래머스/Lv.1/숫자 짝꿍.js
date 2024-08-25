f251850dd86d40e6c953814c0f48fd75ed7e52ff
son-young-jin <sdudwls128@naver.com>
2024-08-25 21:25:42 +0900

    2024 08 25

--- 프로그래머스/Lv.1/숫자 짝꿍.js
+++ 프로그래머스/Lv.1/숫자 짝꿍.js
@@ -1,28 +1,28 @@
 function solution(X, Y) {
+  const len = Math.max(X.length, Y.length);
   const xCount = Array(10).fill(0);
   const yCount = Array(10).fill(0);
   let answer = "";
   let subset = 0;
 
-  const len = Math.max(X.length, Y.length);
   for (let i = 0; i < len; i++) {
     if (X[i]) { xCount[X[i]]++; }
     if (Y[i]) { yCount[Y[i]]++; }
   }
-  // [...X].forEach(x => xCount[x]++);
-  // [...Y].forEach(y => yCount[y]++);
 
   for (let i = 9; i > -1; i--) {
     subset = Math.min(xCount[i], yCount[i]);
-    answer += String(i).repeat(subset);
+    answer = answer + String(i).repeat(subset);
   }
 
-  return answer.length ? String(BigInt(answer)) : "-1";
+  if (!answer.length) return "-1";
+  if (!Number(answer)) return "0";
+  return answer;
+  // return answer.length ? String(BigInt(answer)) : "-1";
 }
 
-// console.log(solution("100", "2345")); // "-1"
-// console.log(solution("100", "203045")); // "0"
-// console.log(solution("100", "123450"))	// "10"
-// console.log(solution("12321", "42531"))	// "321"
-// console.log(solution("5525", "1255"))	// "552"
-console.log(solution("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111123111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111", "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111131111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")); // 10
+console.log(solution("100", "2345")); // "-1"
+console.log(solution("100", "203045")); // "0"
+console.log(solution("100", "123450"))	// "10"
+console.log(solution("12321", "42531"))	// "321"
+console.log(solution("5525", "1255"))	// "552"
