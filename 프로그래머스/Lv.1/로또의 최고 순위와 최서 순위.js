23f65e66f3defa55b14d5b86575052e09f965872
son-young-jin <sdudwls128@naver.com>
2024-08-26 00:16:46 +0900

    2024 08 26

--- 프로그래머스/Lv.1/로또의 최고 순위와 최서 순위.js
+++ 프로그래머스/Lv.1/로또의 최고 순위와 최서 순위.js
@@ -1,7 +1,7 @@
 function solution(lottos, win_nums) {
   const zeroCount = lottos.filter(x => x === 0).length;
-  const matchCount = lottos.filter(num => win_nums.includes(num)).length;
-  const max = 7 - (matchCount + zeroCount);
+  const matchCount = win_nums.filter(x => lottos.includes(x)).length;
+  const max = Math.min(7 - matchCount - zeroCount, 6);
   const min = Math.min(7 - matchCount, 6);
   return [max, min];
 }
@@ -9,3 +9,4 @@ function solution(lottos, win_nums) {
 console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19])); // [3, 5]
 console.log(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25])); // [1, 6]
 console.log(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35])); // [1, 1]
+console.log(solution([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]));
