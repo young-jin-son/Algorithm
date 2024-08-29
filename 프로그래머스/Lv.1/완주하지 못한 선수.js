7ffc87f6e7f550ecb812873149b8590262c9d4db
son-young-jin <sdudwls128@naver.com>
2024-08-29 12:59:06 +0900

    2024 08 29

---
+++ 프로그래머스/Lv.1/완주하지 못한 선수.js
@@ -0,0 +1,21 @@
+function solution(participant, completion) {
+  const completed = {};
+  let unfinished = undefined;
+
+  for (const player of completion) {
+    completed[player] = (completed[player] || 0) + 1;
+  }
+  for (const player of participant) {
+    if (completed[player]) {
+      completed[player]--;
+    } else {
+      unfinished = player;
+      break;
+    }
+  }
+  return unfinished;
+}
+
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"])); // "leo"
+console.log(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])); // "vinko"
+console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])); // "mislav"
