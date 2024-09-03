95e3f0b1979eba8fbfd4ed758a638b55e73501e4
son-young-jin <sdudwls128@naver.com>
2024-09-03 13:31:25 +0900

    2024 09 03

--- 프로그래머스/Lv.3/등굣길.js
+++ 프로그래머스/Lv.3/등굣길.js
@@ -1,30 +1,28 @@
 function solution(m, n, puddles) {
-  const graph = Array.from({ length: n + 1 }, () => Array.from({ length: m + 1 }, () => 0));
+  const graph = Array.from({ length: m + 1 }, () => Array.from({ length: n + 1 }, () => 0));
   const divider = 1_000_000_007;
   for (const [x, y] of puddles) {
     graph[x][y] = -1;
   }
   graph[1][1] = 1;
 
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= m; j++) {
-      if (i === 1 && j === 1) continue;
-      if (graph[i][j] === -1) continue;
-      if (graph[i - 1][j] === -1 && graph[i][j - 1] === -1) {
-        continue;
-      } else if (graph[i - 1][j] === -1) {
-        graph[i][j] = graph[i][j - 1];
-      } else if (graph[i][j - 1] === -1) {
-        graph[i][j] = graph[i - 1][j];
-      } else {
-        graph[i][j] = (graph[i - 1][j] + graph[i][j - 1]) % divider;
+  for (let x = 1; x <= m; x++) {
+    for (let y = 1; y <= n; y++) {
+      if (graph[x][y] === -1) continue;
+      let tmp = 0;
+      if (graph[x - 1][y] !== -1) {
+        tmp += graph[x - 1][y];
       }
+      if (graph[x][y - 1] !== -1) {
+        tmp += graph[x][y - 1];
+      }
+      graph[x][y] += tmp % divider;
     }
   }
 
-  return graph[n][m];
+  return graph[m][n];
 }
 
 console.log(solution(4, 3, [[2, 2]])) // 4
-console.log(solution(6, 4, [[1, 2], [3, 4], [4, 5]])); // 2
+console.log(solution(6, 4, [[1, 2], [3, 4], [4, 5]])); // 31
 
