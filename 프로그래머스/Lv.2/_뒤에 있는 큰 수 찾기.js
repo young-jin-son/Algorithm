a55f0a8346a2d7cc4eb9d49683da6dfa432edde4
son-young-jin <sdudwls128@naver.com>
2024-09-08 17:30:38 +0900

    2024 09 08

---
+++ 프로그래머스/Lv.2/_뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+  const n = numbers.length;
+  const answer = new Array(n).fill(-1);
+  const stack = [];
+
+  for (let i = 0; i < n; i++) {
+    while (stack.length > 0 && numbers[i] > numbers[stack.at(-1)]) {
+      const idx = stack.pop();
+      answer[idx] = numbers[i];
+    }
+    stack.push(i);
+  }
+
+  return answer;
+}
+
+console.log(solution([2, 3, 3, 5])); // [3, 5, 5, -1] 
+console.log(solution([9, 1, 5, 3, 6, 2])); // [-1, 5, 6, 6, -1, -1]
+console.log(solution([4, 3, 2, 1, 2, 3])); // [-1, -1, 3, 2, 3, -1]
+// console.log(solution()); // 
