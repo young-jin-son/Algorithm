6eda0bb5a540e401f856b85669b3d07745ae0d1c
son-young-jin <sdudwls128@naver.com>
2024-09-12 11:06:59 +0900

    2024 09 12

--- 프로그래머스/Lv.2/주식가격.js
+++ 프로그래머스/Lv.2/주식가격.js
@@ -1,8 +1,17 @@
 function solution(prices) {
+  const answer = [];
+  for (let i = 0; i < prices.length; i++) {
+    let term = 0;
+    for (let j = i + 1; j < prices.length; j++) {
+      term++;
+      if (prices[i] > prices[j]) {
+        break;
+      }
+    }
+    answer.push(term);
+  }
 
+  return answer;
 }
 
-console.log(solution([1, 2, 3, 2, 3])); // [4,3,1,1,0] 
-// console.log(solution()); // 
-// console.log(solution()); // 
-// console.log(solution()); // 
+console.log(solution([1, 2, 3, 2, 3])); // [4,3,1,1,0]
