9ad7ab2670da6f09de8ca5b015b15db58df89863
son-young-jin <sdudwls128@naver.com>
2024-09-16 09:21:06 +0900

    2024 09 16

---
+++ 프로그래머스/Lv.3/_디스크 컨트롤러.js
@@ -0,0 +1,32 @@
+function solution(jobs) {
+  const n = jobs.length;
+  let i = 0;
+  let time = 0;
+  let sum = 0;
+  const pq = [];
+
+  jobs = jobs.sort((a, b) => a[0] - b[0]);
+
+  while (i < n || pq.length) {
+    while (i < n && jobs[i][0] <= time) {
+      pq.push(jobs[i++]);
+    }
+
+    if (pq.length) {
+      pq.sort((a, b) => b[1] - a[1]);
+      const [start, duration] = pq.pop();
+      time += duration;
+      sum += time - start;
+    } else {
+      time = jobs[i][0];
+    }
+  }
+
+  return Math.floor(sum / n); // 평균 대기 시간을 반환
+}
+
+
+console.log(solution([[0, 3], [1, 9], [2, 6]])); // 9
+// console.log(solution()); // 
+// console.log(solution()); // 
+// console.log(solution()); // 
