34caf2b1e123bbbe003d947d814ead376eeb4a9e
son-young-jin <sdudwls128@naver.com>
2024-11-14 15:40:30 +0900

    2024 11 14

---
+++ 프로그래머스/Lv.2/124 나라의 숫자.js
@@ -0,0 +1,28 @@
+function solution(n) {
+  const dp = [1];
+  while (dp.at(-1) <= n) {
+    dp.push(dp.at(-1) * 3 + 1);
+  }
+  dp.pop();
+
+  let answer = Array(dp.length).fill(1);
+  let i = 0;
+  let cur = dp.at(-1);
+  let addNum = dp.at(-i - 1) - dp.at(-i - 2) || 1;
+
+  while (cur !== n && i < answer.length) {
+    while (cur + addNum <= n) {
+      cur += addNum;
+      answer[i] *= 2;
+    }
+    i += 1;
+    addNum = dp.at(-i - 1) - dp.at(-i - 2) || 1;
+  }
+
+  return answer.join("")
+}
+
+console.log(solution(1)); // 1
+console.log(solution(2)); // 2
+console.log(solution(3)); // 4
+console.log(solution(4)); // 11
