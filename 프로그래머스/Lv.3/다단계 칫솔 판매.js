5a8157d9ba041780a736e3d5d3f6887133c2de30
son-young-jin <sdudwls128@naver.com>
2025-03-27 20:49:12 +0900

    2025 03 27

---
+++ 프로그래머스/Lv.3/다단계 칫솔 판매.js
@@ -0,0 +1,23 @@
+function solution(enroll, referral, seller, amount) {
+  const answer = [];
+  const refer = {};
+  const profit = {};
+
+  enroll.forEach((e, i) => refer[e] = referral[i]);
+
+  for (let i = 0; i < seller.length; i++) {
+    let [s, p] = [seller[i], amount[i] * 100];
+    let [give, take] = [0, 0];
+
+    while (refer[s]) {
+      give = Math.floor(p * 0.1);
+      take = p - give;
+      profit[s] = (profit[s] || 0) + take;
+      if (take < 1) break;
+      p = give;
+      s = refer[s];
+    }
+  }
+
+  return enroll.map((e) => profit[e] || 0);
+}
